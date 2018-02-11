39bc711ca7a7e48061ca1640d1dee67391e98061
colin liao <colinliao@126.com>
2018-02-11 16:36:09 +0800

    update spider for baidu tieba

--- try_baidu_tieba.py
+++ try_baidu_tieba.py
@@ -4,24 +4,11 @@
 # @Author  : colin liao (colinliao@126.com)
 # @Link    : ${link}
 # @Version : $Id$
-
+import sys
+import io
 import requests
 from bs4 import BeautifulSoup
 
-
-def get1_html(url):
-    try:
-        r = requests.get(url, timeout=30)
-        # print(r.content)
-        r.raise_for_status()
-
-        # 这里我们知道百度贴吧的编码是utf-8，所以手动设置的。爬去其他的页面时建议使用：
-        # r.endcodding = r.apparent_endconding
-        r.encoding = 'utf-8'
-        print(r.text)
-        return r.content
-    except:
-        return " ERROR "
 
 def get_html(url):
     try:
@@ -30,9 +17,11 @@ def get_html(url):
         # 这里我们知道百度贴吧的编码是utf-8，所以手动设置的。爬去其他的页面时建议使用：
         # r.endcodding = r.apparent_endconding
         r.encoding = 'utf-8'
+        # print(r.text)
         return r.text
     except:
         return " ERROR "
+
 
 def get_content(url):
     # 初始化一个列表来保存所有的帖子信息：
@@ -44,8 +33,11 @@ def get_content(url):
 
     soup = BeautifulSoup(html, 'lxml')
 
+    # print(soup.title)
+
     liTags = soup.find_all('li', attrs={'class': ' j_thread_list clearfix'})
 
+    # print(liTags[0].prettify)
 
     # 通过循环找到每个帖子里的我们需要的信息：
     for li in liTags:
@@ -57,23 +49,23 @@ def get_content(url):
             # 开始筛选信息，保存到字典中
             comment['title'] = li.find(
                 'a', attrs={'class': 'j_th_tit '}).text.strip()
-            print(content['title'])
+            # print(comment['title'])
 
             comment['link'] = "http://tieba.baidu.com" + \
                 li.find('a', attrs={'class': 'j_th_tit '})['href']
-            print(content['link'])
+            # print(comment['link'])
 
-            comment['author'] = li.find(
-                "span", sttrs={'class': 'tb_icon_author '}).text.strip()
-            print(comment['author'])
+            comment['name'] = li.find(
+                'span', attrs={'class': 'tb_icon_author '}).text.strip()
+            # print(comment['name'])
 
             comment['time'] = li.find(
-                'span', sttrs={'class': 'pull-right is_show_create_time'}).text.strip()
-            print(comment['time'])
+                'span', attrs={'class': 'pull-right is_show_create_time'}).text.strip()
+            # print(comment['time'])
 
             comment['reply_num'] = li.find(
-                'span', sttrs={'class': 'threadlist_rep_num center_text'}).text.strip()
-            print(comment['reply_num'])
+                'span', attrs={'class': 'threadlist_rep_num center_text'}).text.strip()
+            # print(comment['reply_num'])
 
             comments.append(comment)
 
@@ -83,21 +75,34 @@ def get_content(url):
     return comments
 
 
-
+def Out2File(dict):
+    with open('TTBT.txt', 'a+') as f:
+        for comment in dict:
+            f.write('标题： {} \t 链接：{} \t 发帖人：{} \t 发帖时间：{} \t 回复数量： {} \n'.format(
+                comment['title'], comment['link'], comment['name'], comment['time'], comment['reply_num']))
+        print('当前页面爬取完成')
 
 
 def main(base_url, deep):
+
+    sys.stdout = io.TextIOWrapper(
+        sys.stdout.buffer, encoding='utf8')  # 改变标准输出的默认编码
+
     url_list = []
     for i in range(0, deep):
         url_list.append(base_url + '&pn=' + str(50 * i))
         print(url_list[i])
 
     for url in url_list:
-        get_content(url)
+        content = get_content(url)
+        Out2File(content)
+
+    print('所有的信息都已经保存完毕！')
+
 
 base_url = 'http://tieba.baidu.com/f?kw=%E7%94%9F%E6%B4%BB%E5%A4%A7%E7%88%86%E7%82%B8&ie=utf-8'
 
-deep = 1
+deep = 3
 
 if __name__ == '__main__':
     main(base_url, deep)
--- try_requests.py
+++ try_requests.py
@@ -1,43 +1,14 @@
+import io
+import sys
 import requests
 
+# 我们来吧百度的index页面的html源码抓取到本地，并用r变量保存
+# 注意这里，网页前面的 http://一定要写出来，它并不能像真正的浏览器一样帮我们补全http协议
 
-def getHtmlText(url):
-    try:
-        print('get_html start')
-        r = requests.get(url, timeout=30)
-        print('get_html end')
-        print(r.content)
-        # 如果状态码不是200 则应发HTTOError异常
-        r.raise_for_status()
-        # 设置正确的编码方式
-        r.encoding = r.apparent_encoding
-        return r.content
-    except:
-        return "Something Wrong!"
+sys.stdout = io.TextIOWrapper(
+    sys.stdout.buffer, encoding='utf8')  # 改变标准输出的默认编码
 
+r = requests.get("http://www.baidu.com")
 
-def get_html(url):
-    try:
-        print('get_html start')
-        r = requests.get(url, timeout=30)
-        print('get_html end')
-        print(r.content)
-        r.raise_for_status()
-
-        # 这里我们知道百度贴吧的编码是utf-8，所以手动设置的。爬去其他的页面时建议使用：
-        # r.endcodding = r.apparent_endconding
-        r.encoding = 'utf-8'
-        return r.content
-    except:
-        print('ERROR')
-        return " ERROR "
-
-
-def main(url):
-    get_html(url)
-
-
-url = 'http://www.baidu.com'
-
-if __name__ == '__main__':
-    main(url)
+# 将下载到的内容打印一下：
+print(r.text)
